d3989d19f8c1386c8509a70917182197604a3c4e
zhangshoukang <zhangshoukang@fancydigital.com.cn>
2018-12-13 18:14:42 +0800

    add test

--- src/main/java/org/myorg/quickstart/Test.java
+++ src/main/java/org/myorg/quickstart/Test.java
@@ -7,9 +7,35 @@ import org.apache.flink.api.java.operators.DataSource;
 public class Test {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment executionEnvironment = ExecutionEnvironment.getExecutionEnvironment();
-        DataSource<Integer> integerDataSource = executionEnvironment.fromElements(1, 2, 3, 34);
-        AggregateOperator<Integer> sum = integerDataSource.sum(0);
+        DataSource<Tup> tupDataSource = executionEnvironment.fromElements(new Tup(1, 10), new Tup(1, 10), new Tup(2, 20), new Tup(2, 20));
+        AggregateOperator<Tup> sum = tupDataSource.sum(0);
         sum.print();
         executionEnvironment.execute("hahah");
+    }
+
+}
+class Tup{
+    private int id;
+    private int score;
+
+    public Tup(int id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
