452e90d30519636e99e05c57e787b4c96e868649
zhangshoukang <zhangshoukang@fancydigital.com.cn>
2018-12-13 18:01:08 +0800

    add test

--- src/main/java/org/myorg/quickstart/Test.java
+++ src/main/java/org/myorg/quickstart/Test.java
@@ -1,21 +1,15 @@
 package org.myorg.quickstart;
 
-import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
 import org.apache.flink.api.java.operators.DataSource;
-import org.apache.flink.api.java.operators.FilterOperator;
 
 public class Test {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment executionEnvironment = ExecutionEnvironment.getExecutionEnvironment();
         DataSource<Integer> integerDataSource = executionEnvironment.fromElements(1, 2, 3, 34);
-        FilterOperator<Integer> filter = integerDataSource.filter(new FilterFunction<Integer>() {
-            @Override
-            public boolean filter(Integer integer) throws Exception {
-                return integer.equals(2);
-            }
-        });
-        filter.collect();
-        executionEnvironment.execute();
+        AggregateOperator<Integer> sum = integerDataSource.sum(0);
+        sum.print();
+        executionEnvironment.execute("hahah");
     }
 }
